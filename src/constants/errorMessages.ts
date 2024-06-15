4d7abc47929ccf13aa86dacf42bc8e0a4e766849
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-06-16 01:44:36 +0900

    errorMessages 更新

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,18 +1,13 @@
 export const PRISMA_ERROR_CODE = {
-  P2002: "P2002",
-  P2003: "P2003",
-  P2016: "P2016",
-  P2023: "P2023",
-  P2025: "P2025",
+  P2002: "P2002", // "Unique constraint failed on the {constraint}"
+  P2003: "P2003", //"Foreign key constraint failed on the field: {field_name}"
+  P2025: "P2025", //"An operation failed because it depends on one or more records that were required but not found. {cause}"
 };
 
-export const USER_NOT_EXISTS_ERROR_MESSAGE = "ユーザーは存在しません";
 export const USER_EMAIL_DUPLICATED_ERROR_MESSAGE =
   "ユーザーのメールは登録済みです";
 export const USER_AUTHENTICATION_FAILED_ERROR_MESSAGE =
   "ユーザーのメールもしくはパスワードは正しくありません";
-export const API_KEY_TYPE_INCORRECT_ERROR_MESSAGE =
-  "API KEYの種別が正しくありません";
 export const API_KEY_AUTHENTICATION_FAILED_ERROR_MESSAGE =
   "API KEYの認証は失敗しました";
 export const WORKSHOP_NOT_EXISTS_ERROR_MESSAGE =
